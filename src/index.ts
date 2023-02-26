3605fbbca684ca76f23863bf4ab046c67308ae43
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2023-02-26 13:12:36 -0300

    poc V0: add class/decorator based feature toggle

--- src/index.ts
+++ src/index.ts
@@ -38,3 +38,29 @@ testChangeDisabledToggleNewFlow(changeFlow);
 const testNewDisabledToggle = baseToggle(disabledFeature)({newFlow: ()=> console.log("running new flow")})
 // 2nd: enabled feature
 const testNewEnabledToggle = baseToggle("not disabled feature")({newFlow: ()=>console.log("running new flow (not disabled)")});
+
+// Using decorators
+//
+const FeatureToggle = (feature: string)=> {
+  return (target: any, propertyKey: any, descriptor: PropertyDescriptor) => {
+    const realMethod = descriptor.value;
+    if (feature === "teste") {
+     descriptor.value =  ()=> { throw new Error("It's an error")};
+    }
+    console.log(`target: ${JSON.stringify(target)}`);
+    console.log(`propertyKey: ${JSON.stringify(propertyKey)}`);
+    console.log(`descriptor: ${JSON.stringify(descriptor)}`);
+  }
+}
+
+class Target {
+  
+  @FeatureToggle("tes")
+  superMethod() {
+    console.log("Super Method being executed");
+  }
+}
+
+const featureToggleTest = new Target();
+
+featureToggleTest.superMethod();
